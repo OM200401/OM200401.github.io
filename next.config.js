519e18fedd922dd0d0fffa9cb88789654240e6c9
OM200401 <ommistry012004@gmail.com>
2024-01-21 18:40:19 -0800

    made changes to nextjs config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    exportPathMap: function () {
+        return {
+          '/': { page: '/' },
+          // Add other pages as needed
+        };
+      },
 }
 
 module.exports = nextConfig
